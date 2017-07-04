59c8333da906d4423102ede9f7e6b2247af35b80
mazao <mazao@zjrealtech.com>
2017-07-04 18:03:19 +0800

    echars的研究

---
+++ project-name/src/pages/index/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.hello {
+  h1 {
+    transform: rotate(3deg);
+  }
+}
+</style>
