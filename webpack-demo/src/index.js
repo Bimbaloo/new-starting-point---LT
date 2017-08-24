fec7f2b4a527424e5ad4a885b85ae2ab51541041
mazao <mazao@zjrealtech.com>
2017-08-24 17:41:18 +0800

    管理资源

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,19 @@
  import _ from 'lodash';
-
+ import './style.css';
+ import Icon from './icon.png';
+ 
   function component() {
     var element = document.createElement('div');
 
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+// 将图像添加到我们现有的 div。
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
 
     return element;
   }
