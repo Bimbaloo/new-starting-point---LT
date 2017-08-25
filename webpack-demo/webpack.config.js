09a51dba396563a71c1e236f38c64bdf2e08b989
mazao <mazao@zjrealtech.com>
2017-08-25 09:49:49 +0800

    js顺序加载的一些注释

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -7,25 +7,26 @@ module.exports = {
         print: './src/print.js',
         app: './src/index.js'
     },
-    plugins: [
+    plugins: [  //插件
         new CleanWebpackPlugin(['dist']),       //清理dist文件
         new HtmlWebpackPlugin({                 //默认将js绑定到html中
             title: 'Output Management',
-            chunks:['print', 'app'],
-            // chunksSortMode: function (chunk1, chunk2) {
+            chunks: ['print', 'app'],
+            chunksSortMode: 'manual'  //手动排序
+            /* 函数排序方式 */
+            // chunksSortMode: function (chunk1, chunk2) {  
             //     var order = ['print', 'app'];
             //     var order1 = order.indexOf(chunk1.names[0]);
             //     var order2 = order.indexOf(chunk2.names[0]);
             //     return order1 - order2;  
             // }
-            chunksSortMode: 'manual'  //手动
         })
     ],
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
-    module: {
+    module: {  //loader
         rules: [
             {
                 test: /\.css$/,
