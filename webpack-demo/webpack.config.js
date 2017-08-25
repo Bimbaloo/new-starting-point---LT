8c0ac3e5cea7b41dc188eb1bc87de1f7db81cb9d
mazao <mazao@zjrealtech.com>
2017-08-25 10:57:55 +0800

    webpack--指南--模块热更新部分(完)

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,36 +1,30 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');     //自动将js绑定在html的插件
+const HtmlWebpackPlugin = require('html-webpack-plugin');     //自动将[css][js][title]等信息绑定在html的插件
 const CleanWebpackPlugin = require('clean-webpack-plugin');   //自动清理指定[dist]的插件
 const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
+        app: './src/index.js'
     },
     devtool: 'inline-source-map',               // 启用source map
     devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        host: '127.0.0.1',
-        port: 9000,
-        open: true,
-        openPage: 'index.html',
+        contentBase: path.join(__dirname, "dist"), // 默认打开路径
+        host: '127.0.0.1',                      // IP路径
+        port: 9000,                             // 端口号
+        open: true,                             // 是否自动打开
+        openPage: 'index.html',                 // 自动打开页面
         compress: true,                         // 启动gzip压缩
+        hot: true                               // 热更新模块
     },
     plugins: [  //插件
         new CleanWebpackPlugin(['dist']),       //清理dist文件
-        new HtmlWebpackPlugin({                 //默认将js绑定到html中
+        new HtmlWebpackPlugin({                 //自动将j[css][js][title]等信息绑定到生产html的插件
             title: 'Output Management',
             chunks: ['print', 'app'],
             chunksSortMode: 'manual'  //手动排序
-            /* 函数排序方式 */
-            // chunksSortMode: function (chunk1, chunk2) {  
-            //     var order = ['print', 'app'];
-            //     var order1 = order.indexOf(chunk1.names[0]);
-            //     var order2 = order.indexOf(chunk2.names[0]);
-            //     return order1 - order2;  
-            // }
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin() // webpack内置的hot插件
     ],
     output: {
         filename: '[name].js',
