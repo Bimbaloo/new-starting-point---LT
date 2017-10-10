14b52f7477ac6ab39f4f7022f321b2cdfad75cd1
mazao <mazao@zjrealtech.com>
2017-10-10 17:00:20 +0800

    easyTable实验

---
+++ easytable/src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import 'vue-easytable/libs/themes-base/index.css'
+import {VTable,VPagination} from 'vue-easytable'
+
+Vue.component(VTable.name, VTable)
+Vue.component(VPagination.name, VPagination)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
