09302b14b3038e4ace29c6ffa7a5e20224eb4b85
mazao <mazao@zjrealtech.com>
2017-10-23 10:58:06 +0800

    各种东西的验证

--- ag-grid-vue-example/src/App.vue
+++ ag-grid-vue-example/src/App.vue
@@ -1,27 +1,53 @@
 <template>
   <div id="app">
-    <history :msg='msg'></history>
+    <!-- <history :msg='msg'></history> -->
     <!-- <button style="width:100px;height:20px" @click="a"></button> -->
+    <click></click>
+    <dbclick></dbclick>
+    <div style='border:1px solid red'>
+      <button @click="listShow()">danji</button> 
+       <button @dblclick="legendClickHandle()">shuangjia</button> 
+    </div> 
   </div>
 </template>
 
 <script>
 import history from './components/history'
+import click from './components/click'
+import dbclick from './components/dbclick'
 import { bus } from './assets/bus.js'
 export default {
   name: 'app',
   data () {
     return {
-      msg: '1111'
+      msg: '1111',
+      timeout: null 
     }
   },
   components: {
-    history
+    history,
+    click,
+    dbclick
   },
   methods: {
     a(){
       bus.$emit('id-selected', 1)
-    }
+    },
+    clear() {   
+      if (this.timeout) {  
+         window.clearTimeout(this.timeout);  
+      }  
+    },
+    listShow() {
+      this.clear();  
+      this.timeout = window.setTimeout(()=> {
+        return console.log("1") 
+      }, 300);  
+    },
+    legendClickHandle() {
+      window.clearTimeout(this.timeout);
+      console.log("2")
+    }  
   }
 }
 </script>
---
+++ ag-grid-vue-example/src/components/dbclick.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="hello">
+      <button v-finger:double-tap="doubleTap">双击</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'hello',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+        ok: true
+    }
+  },
+  created() {
+
+  },
+  computed: {
+
+  },
+  methods: {
+      doubleTap() { console.log('双击'); },
+      singleTap() { console.log('单击'); }
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+.history-box {
+  position: fixed;
+  background-color: rgba(255, 255, 255, 0.1);
+  height: 100%;
+  left: 0;
+  top: 0;
+  .el-icon-arrow-left,
+  .el-icon-arrow-right {
+    position: absolute;
+    right: 20px;
+    top: 20px;
+    color: #fff;
+    cursor: pointer;
+  }
+  .history-panal {
+    box-sizing: border-box;
+    margin-top: 50px;
+    padding-left: 15px;
+    padding-right: 15px;
+    opacity: 1;
+    transition: opacity .8s;
+    &>h2 {
+      color: #fff;
+      text-align: center;
+      margin-bottom: 20px;
+    }
+    .history-content {
+      margin-bottom: 30px;
+      >li {
+        margin: 20px;
+      }
+      .ecorded-time {
+        &>h3 {
+          font-size: 14px;
+          color: 14px;
+          text-align: left;
+          color: #ffffff;
+        }
+      }
+      .history-messages-everyday {
+        .information-record {
+          display: flex;
+          color: #e5e5e5;
+          font-size: 12px;
+          line-height: 15px;
+          cursor: pointer;
+          &:hover {
+            color: #42af8f;
+            .circle {
+              border-color: #42af8f;
+              position: relative;
+            }
+            .circle:before {
+              content: '';
+              border: 2px solid #42af8f;
+              border-radius: 50%;
+              width: 2px;
+              height: 2px;
+              display: inline-block;
+              position: absolute;
+              left: 2px;
+              top: 2px;
+            }
+          }
+          &>li {
+            margin-right: 10px;
+            .circle {
+              border: 2px solid #fff;
+              border-radius: 50%;
+              width: 10px;
+              height: 10px;
+              display: inline-block;
+            }
+          }
+          .records {
+            flex: 1;
+            .detail-record-box {
+              display: flex;
+              flex-wrap: wrap;
+              .detail-record {
+                margin-right: 10px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+.max-history-box {
+  transition: width .8s ease;
+  width: 400px;
+}
+.min-history-box {
+  transition: width .5s ease;
+  width: 50px;
+}
+ul {
+  padding: 0;
+}
+
+li {
+  list-style: none;
+}
+
+</style>
