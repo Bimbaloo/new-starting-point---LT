4809e8de312c95e04720493c9d1a3bf39a3adabd
mazao <mazao@zjrealtech.com>
2017-11-10 17:56:03 +0800

    各种验证

--- vuecli-example/src/App.vue
+++ vuecli-example/src/App.vue
@@ -1,67 +1,107 @@
 <template>
   <div id="app">
-    <!-- <history :msg='msg'></history> -->
-    <!-- <button style="width:100px;height:20px" @click="a"></button> -->
-    <click></click>
-    <dbclick></dbclick>
-    <div style='border:1px solid red'>
-      <button @click="listShow()">danji</button> 
-       <button @dblclick="legendClickHandle()">shuangjia</button> 
-    </div> 
+    <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="密码" prop="pass">
+        <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="年龄" prop="age">
+        <el-input v-model.number="ruleForm2.age"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-import history from './components/history'
-import click from './components/click'
-import dbclick from './components/dbclick'
-import { bus } from './assets/bus.js'
+import history from "./components/history";
+import click from "./components/click";
+import dbclick from "./components/dbclick";
+import { bus } from "./assets/bus.js";
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
+    var checkAge = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("年龄不能为空"));
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error("请输入数字值"));
+        } else {
+          if (value < 18) {
+            callback(new Error("必须年满18岁"));
+          } else {
+            callback();
+          }
+        }
+      }, 1000);
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.ruleForm2.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm2.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
     return {
-      msg: '1111',
-      timeout: null 
-    }
-  },
-  components: {
-    history,
-    click,
-    dbclick
+      ruleForm2: {
+        pass: "",
+        checkPass: "",
+        age: ""
+      },
+      rules2: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
+        age: [{ validator: checkAge, trigger: "blur" }]
+      }
+    };
   },
   methods: {
-    a(){
-      bus.$emit('id-selected', 1)
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     },
-    clear() {   
-      if (this.timeout) {  
-         window.clearTimeout(this.timeout);  
-      }  
-    },
-    listShow() {
-      this.clear();  
-      this.timeout = window.setTimeout(()=> {
-        return console.log("1") 
-      }, 300);  
-    },
-    legendClickHandle() {
-      window.clearTimeout(this.timeout);
-      console.log("2")
-    }  
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
   }
-}
+};
 </script>
 
 <style>
-html,body, #app {
-    height: 100%;
-    width: 100%;
+html,
+body,
+#app {
+  height: 100%;
+  width: 100%;
 }
 body {
-    background-color: #f2f2f2;
-    background: url(./assets/bg.jpg);
-    background-size: cover;
-    margin: 0;
-  }
-
+  background-color: #f2f2f2;
+  background: url(./assets/bg.jpg);
+  background-size: cover;
+  margin: 0;
+}
 </style>
--- vuecli-example/src/main.js
+++ vuecli-example/src/main.js
@@ -5,11 +5,7 @@ import App from './App'
 import axios from 'axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
-import AlloyFinger from 'alloyfinger/alloy_finger.js'
-import AlloyFingerVue from 'alloyfinger/vue/alloy_finger.vue.js'
 
-//window.AlloyFinger = AlloyFinger
-Vue.use(AlloyFingerVue, { AlloyFinger:  AlloyFinger });
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
