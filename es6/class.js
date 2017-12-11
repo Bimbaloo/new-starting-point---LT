b4900ccaa2cb9795d831edb2434bcd3a3a558811
mazao <mazao@zjrealtech.com>
2017-12-11 10:51:10 +0800

    class的练习

---
+++ es6/class.js
@@ -0,0 +1,28 @@
+class Point {
+  constructor (x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  toString () {
+    return '(' + this.x + ', ' + this.y + ')'
+  }
+}
+
+
+class A {
+  constructor(name,age) {
+    this.name = name
+    this.age = age
+  }
+}
+A.prototype.x = 2;
+
+class B extends A {
+  constructor() {
+    super();
+    console.log(super.x) // 2
+  }
+}
+
+let b = new B();
