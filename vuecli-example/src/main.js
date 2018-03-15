67fd5930fcad2fc081110a4c2ab4bb5867f69d92
mazao <mazao@zjrealtech.com>
2018-03-15 09:55:28 +0800

    各种练习吧

--- vuecli-example/src/main.js
+++ vuecli-example/src/main.js
@@ -5,6 +5,18 @@ import App from './App'
 import axios from 'axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+import ECharts from 'vue-echarts/components/ECharts.vue'
+
+// 手动引入 ECharts 各模块来减小打包体积
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/chart/line'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/title'
+
+// 注册组件后即可使用
+Vue.component('chart', ECharts)
+
 Vue.use(ElementUI)
 
 /* 拖动组件 */
